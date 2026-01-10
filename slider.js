f5624343223f6ecb7842a45b1bb201dd987bbacd
Rijuan <mohammadrijuan2529@gmail.com>
2026-01-11 00:13:27 +0600

    checking

--- slider.js
+++ slider.js
@@ -4,25 +4,26 @@ const upBtn = document.querySelector(".arrow-up");
 const downBtn = document.querySelector(".arrow-down");
 const completeBg = document.querySelector(".complete-bg");
 const fruitItems = document.querySelectorAll(".fruit-list li");
-const circleImg = document.querySelector(".circle-img");
 const semiCircle = document.querySelector(".semi-circle-down");
-
 
 let currentIndex = 0;
 
-function getSliderHeight() {
+
+function getSliderHeight(index) {
+
+  if (index === 0) return 220;
+  if (index === 1) return 310;
+
   if (window.matchMedia("(max-width: 576px)").matches) {
-    return 240;     
+    return 240;
   } 
   else if (window.matchMedia("(max-width: 768px)").matches) {
-    return 255;       
+    return 255;
   } 
   else {
-    return 315;    
+    return 340;
   }
 }
-
-
 
 const bgColors = [
   "#443747",
@@ -34,31 +35,35 @@ const bgColors = [
 ];
 
 function updateArrowOpacity() {
-  upBtn.style.opacity = currentIndex === 0 ? "0.6" : "1";
+  upBtn.style.opacity = currentIndex === 0 ? "0.5" : "1";
   downBtn.style.opacity =
-    currentIndex === sliders.length - 1 ? "0.6" : "1";
+    currentIndex === sliders.length - 1 ? "0.5" : "1";
 }
-
-
-function updateSlider() {
-  const sliderHeight = getSliderHeight();
-
-  fruitsContainer.style.transform =
-    `translateY(-${currentIndex * sliderHeight}px)`;
-
-  completeBg.style.backgroundColor = bgColors[currentIndex];
-
-  const step = 22.5;
-  semiCircle.style.setProperty("--fill", `${150 + currentIndex * step}deg`);
-
-  setActiveFruit();
-  updateArrowOpacity();
-}
-
 
 function setActiveFruit() {
   fruitItems.forEach(item => item.classList.remove("active"));
   fruitItems[currentIndex].classList.add("active");
+}
+
+function updateSlider() {
+  let moveY = 0;
+
+  for (let i = 0; i < currentIndex; i++) {
+    moveY += getSliderHeight(i);
+  }
+
+  fruitsContainer.style.transform = `translateY(-${moveY}px)`;
+
+  completeBg.style.backgroundColor = bgColors[currentIndex];
+
+  const step = 22.5;
+  semiCircle.style.setProperty(
+    "--fill",
+    `${150 + currentIndex * step}deg`
+  );
+
+  setActiveFruit();
+  updateArrowOpacity();
 }
 
 
@@ -68,7 +73,6 @@ fruitItems.forEach((item, index) => {
     updateSlider();
   });
 });
-
 
 downBtn.addEventListener("click", () => {
   if (currentIndex < sliders.length - 1) {
@@ -83,5 +87,7 @@ upBtn.addEventListener("click", () => {
     updateSlider();
   }
 });
+
+window.addEventListener("resize", updateSlider);
 
 updateSlider();
